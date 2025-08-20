2bd0dff70a7714582aac092bcddc72bd9ad90ed2
UltraLionFr <ultralionfr@gmail.com>
2025-08-20 23:28:07 +0200

    chore: update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-4 left-1/2 -translate-x-1/2 text-xs text-neutral-400 flex items-center gap-1">
       <span>Made with</span>
-      <FaHeart className="text-red-500 animate-pulse" />
+      <FaHeart className="text-red-500" />
       <span>by</span>
       <a
         href="https://ultralion.xyz"
